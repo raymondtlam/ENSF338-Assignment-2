ec41b17f5f44387cd0ed888a04094ac8ae6761e2
raymondtlam <113803089+raymondtlam@users.noreply.github.com>
2023-02-09 19:08:44 -0700

    Delete ex1.3.py

--- ex1.3.py
+++
@@ -1,9 +0,0 @@
-def optimized_fib(n,cache = {}):
-    if n in cache:
-        return cache[n]
-
-    if n == 0 or n == 1:
-        return n
-        
-    cache[n] = optimized_fib(n-1) + optimized_fib(n-2)
-    return cache[n] 
